cd7508218bc5ac0bd073a96240e4c6840ba20c7c
mikem <mikem.exe@gmail.com>
2017-09-24 20:49:25 -0600

    Take the user directly to the last playing video when the app launches

--- app/src/main/java/com/mymikemiller/chronoplayer/ChannelSearchableActivity.kt
+++ app/src/main/java/com/mymikemiller/chronoplayer/ChannelSearchableActivity.kt
@@ -6,11 +6,13 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.app.SearchManager
+import android.content.Context
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.SimpleAdapter
 import com.google.android.youtube.player.YouTubeApiServiceUtil
+import com.mymikemiller.chronoplayer.util.Channels
 import com.mymikemiller.chronoplayer.yt.YouTubeAPI
 
 
@@ -24,7 +26,28 @@ class ChannelSearchableActivity : ListActivity() {
 
         handleIntent(intent)
 
-        onSearchRequested()
+        // Find the last watched ChannelId in SharedPreferences. If it's there, use the new channel
+        // database to launch the MainActivity specifying the channel in the intent. If it's
+        // not there, launch the channel selector screen {this activity's search screen)
+        val preferences = getSharedPreferences(getString(R.string.sharedPrefsName), Context.MODE_PRIVATE)
+        val channelId = preferences.getString(getString(R.string.launchChannel), "")
+
+        if (channelId != "") {
+            // We have a last watched channel, so launch the MainActivity
+            // Search the database for the specified channelId
+            // There should definitely be a channel in Channels for the channelId stored in
+            // sharedPreferences, but if there isn't we just continue loading
+            val channel = Channels.getChannel(applicationContext, channelId)
+            if (channel != null) {
+                launchMainActivity(channel)
+            }
+        } else {
+            // We couldn't find a last-watched-channel (launch channel) in the shared
+            // preferences, so pop up the search dialog
+            onSearchRequested()
+            //TODO: delete this
+            //finish()
+        }
     }
 
     override fun onNewIntent(intent: Intent) {
@@ -49,9 +72,13 @@ class ChannelSearchableActivity : ListActivity() {
     override fun onListItemClick(l: ListView?, v: View?, position: Int, id: Long) {
         super.onListItemClick(l, v, position, id)
         val channel = listAdapter.getItem(position) as Channel
+        launchMainActivity(channel)
+    }
 
-        // Launch the main activity
-        val mainIntent = Intent(this, MainActivity::class.java);
+    // Launch the main activity
+    fun launchMainActivity(channel: Channel) {
+
+        val mainIntent = Intent(this, MainActivity::class.java)
         mainIntent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT)
         mainIntent.putExtra("channel", channel)
         startActivity(mainIntent)
