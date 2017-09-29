e330a88af0d2b45327aee2d17da021903275b8a2
mikem <mikem.exe@gmail.com>
2017-09-28 19:40:36 -0600

    Channel search now launches correctly when launched from settings

--- app/src/main/java/com/mymikemiller/chronoplayer/ChannelSearchActivity.kt
+++ app/src/main/java/com/mymikemiller/chronoplayer/ChannelSearchActivity.kt
@@ -28,8 +28,8 @@ class ChannelSearchActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_channel_search)
 
-        mListView = findViewById<ListView>(R.id.listView)
         val channels = mutableListOf<Channel>()
+        mListView = findViewById<ListView>(R.id.listView)
         mListView.setAdapter(ChannelAdapter(this, channels))
 
         mSearchView = findViewById<SearchView>(R.id.searchView)
@@ -73,24 +73,29 @@ class ChannelSearchActivity : Activity() {
             }
         }
 
-        // Find the last watched ChannelId in SharedPreferences. If it's there, use the channel
-        // database to launch the MainActivity specifying the channel in the intent. If it's
-        // not there, load this view and search
-        val preferences = getSharedPreferences(getString(R.string.sharedPrefsName), Context.MODE_PRIVATE)
-        val channelId = preferences.getString(getString(R.string.launchChannel), "")
 
-        if (channelId != "") {
-            // We have a last watched channel, so launch the MainActivity
-            // Search the database for the specified channelId
-            // There should definitely be a channel in Channels for the channelId stored in
-            // sharedPreferences, but if there isn't we just continue loading
-            val channel = Channels.getChannel(applicationContext, channelId)
-            if (channel != null) {
-                launchMainActivity(channel)
+        if (!intent.hasExtra(getString(R.string.launchedFromSettings))) {
+            // We were not launched from settings (we loaded the app for the first time), so next
+            // we see if we have a preference for the last channel we watched. Find the last
+            // watched ChannelId in SharedPreferences. If it's there, use the channel
+            // database to launch the MainActivity specifying the channel in the intent. If it's
+            // not there, load this view and let the user search
+            val preferences = getSharedPreferences(getString(R.string.sharedPrefsName), Context.MODE_PRIVATE)
+            val channelId = preferences.getString(getString(R.string.launchChannel), "")
+
+            if (channelId != "") {
+                // We have a last watched channel, so launch the MainActivity
+                // Search the database for the specified channelId
+                // There should definitely be a channel in Channels for the channelId stored in
+                // sharedPreferences, but if there isn't we just continue loading
+                val channel = Channels.getChannel(applicationContext, channelId)
+                if (channel != null) {
+                    launchMainActivity(channel)
+                }
+            } else {
+                // We couldn't find a last-watched-channel (launch channel) in the shared
+                // preferences, so simply allow this activity to load
             }
-        } else {
-            // We couldn't find a last-watched-channel (launch channel) in the shared
-            // preferences, so simply allow this activity to load
         }
     }
 
