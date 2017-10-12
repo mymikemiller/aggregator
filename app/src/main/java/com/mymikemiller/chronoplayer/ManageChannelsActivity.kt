c34d954c4e6296d9d79bfd83654cab5e960729e2
mikem <mikem.exe@gmail.com>
2017-10-12 14:32:25 -0600

    Adding ActionBar with "add" button to manage channels activity

--- app/src/main/java/com/mymikemiller/chronoplayer/ManageChannelsActivity.kt
+++ app/src/main/java/com/mymikemiller/chronoplayer/ManageChannelsActivity.kt
@@ -2,14 +2,45 @@ package com.mymikemiller.chronoplayer
 
 import android.app.Activity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 
 /**
  * Created by mikem on 10/12/2017.
  */
-class ManageChannelsActivity : Activity() {
+class ManageChannelsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_manage_channels)
 
+        // set the acion bar
+        val myToolbar = findViewById<View>(R.id.my_toolbar) as Toolbar
+        setSupportActionBar(myToolbar)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.manage_channels_action_bar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        val id = item.getItemId()
+
+        if (id == R.id.add_channel) {
+            Toast.makeText(this, "Clicked!",
+                    Toast.LENGTH_LONG).show()
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
