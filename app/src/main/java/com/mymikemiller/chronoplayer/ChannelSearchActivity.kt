454be32cf8e05774b9119db0e57b4c8606256345
mikem <mikem.exe@gmail.com>
2017-10-18 09:44:20 -0600

    Preventing a crash on MainAtivity::onActivityResult that occurred due to pressing back from the ChannelSearchActivity. So now we're not allowed to. Shitty solution, but unless we can extend AppCompatActivity, we can't handle a null data ent in onActivityResult.

--- app/src/main/java/com/mymikemiller/chronoplayer/ChannelSearchActivity.kt
+++ app/src/main/java/com/mymikemiller/chronoplayer/ChannelSearchActivity.kt
@@ -33,7 +33,7 @@ class ChannelSearchActivity : Activity() {
         mSearchView.setQueryHint(getString(R.string.channel_search_hint));
         mSearchView.setIconifiedByDefault(false)
 
-        mSearchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
+        mSearchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
                 if (query != null) {
                     YouTubeAPI.fetchChannels(query, { channels ->
@@ -47,7 +47,7 @@ class ChannelSearchActivity : Activity() {
                             })
                         }
                     })
-                    
+
                     // Hide the keyboard
                     val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
                     imm.hideSoftInputFromWindow(mSearchView.getWindowToken(), 0)
@@ -62,7 +62,7 @@ class ChannelSearchActivity : Activity() {
             }
         })
 
-        mListView.onItemClickListener = object: AdapterView.OnItemClickListener {
+        mListView.onItemClickListener = object : AdapterView.OnItemClickListener {
             override fun onItemClick(l: AdapterView<*>?, view: View?, position: Int, id: Long) {
                 val channel = mListView.adapter.getItem(position) as Channel
                 // Add the channel to the database so it's there when we add to PlaylistChannels table
@@ -70,34 +70,6 @@ class ChannelSearchActivity : Activity() {
                 sendResult(channel)
             }
         }
-
-//        if (!intent.hasExtra(getString(R.string.launchedFromSettings))) {
-//            // We were not launched from settings (we loaded the app for the first time), so next
-//            // we see if we have a preference for the last channel we watched. Find the last
-//            // watched ChannelId in SharedPreferences. If it's there, use the channel
-//            // database to launch the MainActivity specifying the channel in the intent. If it's
-//            // not there, load this view and let the user search
-//            val preferences = getSharedPreferences(getString(R.string.sharedPrefsName), Context.MODE_PRIVATE)
-//            val playlistTitle = preferences.getString(getString(R.string.prefPlaylistTitle), "")
-//
-//            if (playlistTitle != "") {
-//                // We have a last watched channel, so launch the MainActivity
-//                // Search the database for the specified channelId
-//                // There should definitely be a channel in Channels for the channelId stored in
-//                // sharedPreferences, but if there isn't we just continue loading
-//                //val channel = Channels.getChannel(applicationContext, channelId)
-//                val i = Intent(this, MainActivity::class.java)
-//                i.putExtra(getString(R.string.extraLaunchPlaylistTitle), playlistTitle)
-//                startActivity(i)
-//                finish()
-//                //if (channel != null) {
-//                //    sendResult(channel)
-////                }
-//            } else {
-//                // We couldn't find a last-watched-channel (launch channel) in the shared
-//                // preferences, so simply allow this activity to load
-//            }
-//        }
     }
 
     // Launch the main activity
@@ -106,5 +78,9 @@ class ChannelSearchActivity : Activity() {
         i.putExtra("channel", channel)
         setResult(RESULT_OK, i)
         finish()
+    }
+
+    override fun onBackPressed() {
+        // Do nothing to prevent a crash in onActivityResult in MainActivity. So we just don't allow back to be pressed.
     }
 }
